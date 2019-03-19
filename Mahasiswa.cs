a4b00fe2a1be0bd28bd9dcd0ea65e398c68739fd
Adi Zaenul <adizaenul2000@gmail.com>
2019-03-19 14:48:42 +0700

    Class and Object

---
+++ Mahasiswa.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Mahasiswa
+    {
+        // properties
+        public string nim { get; set; }
+        public string nama { get; set; }
+        public float ipk { get; set; }
+
+        // method
+        public void registrasi()
+        {
+            Console.WriteLine("Nama : {0}", nama);
+            Console.WriteLine("Nim  : {0}", nim);
+            Console.WriteLine("IPK  : {0}", ipk);
+            Console.WriteLine("Telah melakukan registrasi");
+        }
+
+        public void isikrs()
+        {
+            Console.WriteLine("{0} sedang melakukan proses KRS\n", nama);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Class & Object";
+            // membuat objek dari class mahasiswa
+            Mahasiswa saya = new Mahasiswa();
+            Mahasiswa kamu = new Mahasiswa();
+
+            // mengeset nilai properties objek saya dan kamu
+            saya.nim = "18.11.2269";
+            saya.nama = "Adi Zaenul Mustaqim";
+            saya.ipk = 3.95f;
+
+            kamu.nim = "18.22.2290";
+            kamu.nama = "Kaesang";
+            kamu.ipk = 3.70f;
+
+            // memanggil method registrasi dan isi krs
+            saya.registrasi();
+            saya.isikrs();
+
+            kamu.registrasi();
+            kamu.isikrs();
+
+            Console.ReadKey();
+        }
+    }
+}
